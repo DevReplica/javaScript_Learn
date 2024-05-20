62d9046aebb7d494c318dbcf24fdedc80a9f01fb
DevReplica <warizilori1@gmail.com>
2024-05-20 12:18:28 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,7 @@
+alert(`Total cost(${1 + 1}): $${3 + 5}`)
+console.log("Welcome!");
+console.log("2 + 2");
+console.log("Wel" + "come!");
+console.log("My name is");
+'Hello'
+alert(`Total cost(${1 + 1}): ($${(599 + 295)/100}) \n Thank you \n Come again`);
